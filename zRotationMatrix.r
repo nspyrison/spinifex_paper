896005e61ac956e748fe28a2d69b37d58961ffde
Nicholas Spyrison <spyrison@gmail.com>
2019-04-25 17:09:00 +1000

    fixed matrix multiplication. the 3 angle rotation still not making sense in the same way that Gram schmidt is.

--- zRotationMatrix.r
+++ zRotationMatrix.r
@@ -82,7 +82,8 @@ if (load_funcs) # overwrites the spinifex functions: rotate_manip_space and manu
                     nrow = 3, ncol = 3,
                     byrow = TRUE)
 
-      R <- r_theta * r_phi * r_omega
+      R <- r_theta %*% r_omega%*% r_phi
+      # Order matters, 3rd angle is projection plane rotation.
     }
 
 
@@ -92,83 +93,62 @@ if (load_funcs) # overwrites the spinifex functions: rotate_manip_space and manu
     rotation_space
   }
 
-  # # paste over spinifex function
-  # manual_tour <- function (basis = NULL, manip_var, theta = NULL, phi_min = 0,
-  #                          phi_max = 0.5 * pi, omega=NULL, angle = 0.05)
-  # {
-  #   basis <- as.matrix(basis)
-  #   p <- nrow(basis)
-  #   d <- ncol(basis)
-  #   manip_space <- create_manip_space(basis = basis, manip_var = manip_var)
-  #   if (is.null(theta))
-  #     theta <- atan(basis[manip_var, 2]/basis[manip_var, 1])
-  #   phi_start <- acos(sqrt(basis[manip_var, 1]^2 + basis[manip_var, 2]^2))
-  #   stopifnot(phi_min <= phi_start & phi_max >= phi_start)
-  #   find_path <- function(start, end) {
-  #     mvar_xsign <- -sign(basis[manip_var, 1])
-  #     start <- mvar_xsign * (start - phi_start)
-  #     end <- mvar_xsign * (end - phi_start)
-  #     dist <- abs(end - start)
-  #     steps <- round(dist/angle)
-  #     angle <- dist/steps
-  #     sign <- ifelse(end > start, 1, -1)
-  #     seq(from = start, to = end, by = sign * angle)
-  #   }
-  #   phi_path <- c(find_path(start = phi_start, end = phi_min),
-  #                 find_path(start = phi_min, end = phi_max), find_path(start = phi_max,
-  #                                                                      end = phi_start))
-  #   n_frames <- length(phi_path)
-  #   basis_set <- array(NA, dim = c(p, d, n_frames))
-  #   for (i in 1:n_frames) {
-  #     thisFrame <- my_rotate_manip_space(manip_space = manip_space,
-  #                                        theta = theta, phi = phi_path[i],
-  #                                        omega = omega)
-  #     basis_set[, , i] <- thisFrame[, 1:d]
-  #   }
-  #   attr(basis_set, "manip_var") <- manip_var
-  #   basis_set
-  # }
-
 }
 
 #runif(1, max = 2 * pi)
-my_t <- 1
+#my_t <- 1
+rad_theta <- atan(f_bas[f_mvar, 2]/f_bas[f_mvar, 1])
 my_p <- 2
 f_msp <- create_manip_space(basis = f_bas, manip_var = f_mvar)
 b <- rotate_manip_space(f_msp, theta = my_t, phi = my_p) #omega is Null)
 b0 <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 0)
 # comparing 2 Euler angle rotaion with 3 angle.
 view_basis(b)
-view_basis(b0) # far cry from equal, values too small, orthonormality broken?
+view_basis(o0) # far cry from equal, values too small, orthonormality broken?
 is_orthonormal(b) # neither are orthonormal
-is_orthonormal(b0)
-# changing omega; fixed theta, phi. manip_var = 4
-b05 <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = .5)
-b1  <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 1)
-b15 <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 1.5)
-b2  <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 2)
-b25 <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 2.5)
-b3  <- rotate_manip_space(f_msp, theta = my_p, phi = my_p, omega = 3)
-view_basis(b0)
-view_basis(b05)
-view_basis(b1)
-view_basis(b15)
-view_basis(b2)
-view_basis(b25)
-view_basis(b3)
+is_orthonormal(o0)
+# changing omega; fixed theta, phi. manip_var = 4. rotating whole plane.
+o05 <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = .5)
+o1  <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = 1)
+o15 <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = 1.5)
+o2  <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = 2)
+o25 <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = 2.5)
+o3  <- rotate_manip_space(f_msp, theta = rad_theta, phi = my_p, omega = 3)
+view_basis(o0)
+view_basis(o05)
+view_basis(o1)
+view_basis(o15)
+view_basis(o2)
+view_basis(o25)
+view_basis(o3)
 
 
-# Fix omega and theta, change phi.  manip_var = 4
-my_t <- 1
-my_o <- 4
-p1  <- rotate_manip_space(f_msp, theta = my_p, phi = 1, omega = my_o)
-p15 <- rotate_manip_space(f_msp, theta = my_p, phi = 1.5, omega = my_o)
-p2  <- rotate_manip_space(f_msp, theta = my_p, phi = 2, omega = my_o)
-p25 <- rotate_manip_space(f_msp, theta = my_p, phi = 2.5, omega = my_o)
-p3  <- rotate_manip_space(f_msp, theta = my_p, phi = 3, omega = my_o)
+# Fix omega and theta, change phi. manip_var = 4
+my_o <- 0
+p1  <- rotate_manip_space(f_msp, theta = rad_theta, phi = 1, omega = my_o)
+p15 <- rotate_manip_space(f_msp, theta = rad_theta, phi = 1.5, omega = my_o)
+p2  <- rotate_manip_space(f_msp, theta = rad_theta, phi = 2, omega = my_o)
+p25 <- rotate_manip_space(f_msp, theta = rad_theta, phi = 2.5, omega = my_o)
+p3  <- rotate_manip_space(f_msp, theta = rad_theta, phi = 3, omega = my_o)
 view_basis(p1)
 view_basis(p15)
 view_basis(p2)
 view_basis(p25)
 view_basis(p3)
+# swapping the order of phi and omega, to match order of the variables, not expecting any better.
 
+
+# Fix phi and omega, change theta. manip_var = 4
+my_o <- 0
+my_p <- 2
+t1  <- rotate_manip_space(f_msp, phi = my_p, theta = 1, omega = my_o)
+t15 <- rotate_manip_space(f_msp, phi = my_p, theta = 1.5, omega = my_o)
+t2  <- rotate_manip_space(f_msp, phi = my_p, theta = 2, omega = my_o)
+t25 <- rotate_manip_space(f_msp, phi = my_p, theta = 2.5, omega = my_o)
+t3  <- rotate_manip_space(f_msp, phi = my_p, theta = 3, omega = my_o)
+view_basis(t1)
+view_basis(t15)
+view_basis(t2)
+view_basis(t25)
+view_basis(t3)
+# swapping the order of phi and omega, to match order of the variables, not expecting any better.
